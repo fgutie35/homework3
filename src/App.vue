04e80b977a502b10da28cf54a7c8a58cc4d395ce
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-09-12 14:52:01 -0500

    first commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import {ref} from 'vue'
+
+  const name = ref('')
+
+</script>
+
+<template>
+
+<main class="app">
+
+  <selection class="greeting">
+    <h2 class="title">
+      Welcome back, <input type="text" placeholder="Enter Name" v-model="name">
+    </h2>
+
+  </selection>
+
+  <section class="create todo">
+
+
+  </section>
+
+  <selection class="todo-list">
+
+  </selection>
+
+
+</main>
+
+</template>
+
+
